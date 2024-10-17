oneapi-src/unified-runtime

Rerun generate target after applying suggestions from Github web ui. Move entry points from global ddi table to adapter. Rename the callback function pointer to ur_logger_callback_t.

source/adapters/level_zero/ur_interface_loader.cpp
@@ -44,8 +44,6 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetGlobalProcAddrTable(
   pDdiTable->pfnAdapterRetain = urAdapterRetain;
   pDdiTable->pfnAdapterGetLastError = urAdapterGetLastError;
   pDdiTable->pfnAdapterGetInfo = urAdapterGetInfo;
-  pDdiTable->pfnSetLoggerCallback = urSetLoggerCallback;
-  pDdiTable->pfnSetLoggerCallbackLevel = urSetLoggerCallbackLevel;
 
   return retVal;
 }
@@ -492,6 +490,19 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetProgramExpProcAddrTable(
 
   return UR_RESULT_SUCCESS;
 }
+
+UR_DLLEXPORT ur_result_t UR_APICALL urGetAdapterProcAddrTable(
+    ur_api_version_t version, ur_adapter_dditable_t *pDdiTable) {
+  auto result = validateProcInputs(version, pDdiTable);
+  if (UR_RESULT_SUCCESS != result) {
+    return result;
+  }
+  pDdiTable->pfnSetLoggerCallback = urAdapterSetLoggerCallback;
+  pDdiTable->pfnSetLoggerCallbackLevel = urAdapterSetLoggerCallbackLevel;
+
+  return UR_RESULT_SUCCESS;
+}
+
 #if defined(__cplusplus)
 } // extern "C"
 #endif

oneapi-src/level-zero-unified-runtime

source/adapters/level_zero/ur_interface_loader.cpp
@@ -43,9 +43,20 @@ UR_APIEXPORT ur_result_t UR_APICALL urGetGlobalProcAddrTable(
   pDdiTable->pfnAdapterRetain = ur::level_zero::urAdapterRetain;
   pDdiTable->pfnAdapterGetLastError = ur::level_zero::urAdapterGetLastError;
   pDdiTable->pfnAdapterGetInfo = ur::level_zero::urAdapterGetInfo;
-  pDdiTable->pfnSetLoggerCallback = ur::level_zero::urSetLoggerCallback;
+
+  return result;
+}
+
+UR_APIEXPORT ur_result_t UR_APICALL urGetAdapterProcAddrTable(
+    ur_api_version_t version, ur_adapter_dditable_t *pDdiTable) {
+  auto result = validateProcInputs(version, pDdiTable);
+  if (UR_RESULT_SUCCESS != result) {
+    return result;
+  }
+
+  pDdiTable->pfnSetLoggerCallback = ur::level_zero::urAdapterSetLoggerCallback;
   pDdiTable->pfnSetLoggerCallbackLevel =
-      ur::level_zero::urSetLoggerCallbackLevel;
+      ur::level_zero::urAdapterSetLoggerCallbackLevel;
 
   return result;
 }
@@ -533,6 +544,10 @@ ur_result_t urAdapterGetDdiTables(ur_dditable_t *ddi) {
 
   result = ur::level_zero::urGetGlobalProcAddrTable(UR_API_VERSION_CURRENT,
                                                     &ddi->Global);
+  if (result != UR_RESULT_SUCCESS)
+    return result;
+  result = ur::level_zero::urGetAdapterProcAddrTable(UR_API_VERSION_CURRENT,
+                                                     &ddi->Adapter);
   if (result != UR_RESULT_SUCCESS)
     return result;
   result = ur::level_zero::urGetBindlessImagesExpProcAddrTable(