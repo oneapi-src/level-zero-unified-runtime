oneapi-src/unified-runtime

Add handles to opencl adapter

source/adapters/opencl/event.hpp
@@ -1,0 +1,48 @@
+//===--------- queue.hpp - OpenCL Adapter ---------------------------===//
+//
+// Copyright (C) 2023 Intel Corporation
+//
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM
+// Exceptions. See LICENSE.TXT
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "common.hpp"
+
+#include <vector>
+
+struct ur_event_handle_t_ {
+  using native_type = cl_event;
+  native_type Event;
+  ur_context_handle_t Context;
+  ur_queue_handle_t Queue;
+  std::atomic<uint32_t> RefCount = 0;
+
+  ur_event_handle_t_(native_type Event, ur_context_handle_t Ctx,
+                     ur_queue_handle_t Queue)
+      : Event(Event), Context(Ctx), Queue(Queue) {
+    RefCount = 1;
+    urContextRetain(Context);
+    if (Queue) {
+      urQueueRetain(Queue);
+    }
+  }
+
+  ~ur_event_handle_t_() {
+    urContextRelease(Context);
+    if (Queue) {
+      urQueueRelease(Queue);
+    }
+    clReleaseEvent(Event);
+  }
+
+  uint32_t incrementReferenceCount() noexcept { return ++RefCount; }
+
+  uint32_t decrementReferenceCount() noexcept { return --RefCount; }
+
+  uint32_t getReferenceCount() const noexcept { return RefCount; }
+
+  native_type get() { return Event; }
+};

source/adapters/opencl/sampler.hpp
@@ -1,0 +1,40 @@
+//===--------- sampler.hpp - OpenCL Adapter ---------------------------===//
+//
+// Copyright (C) 2023 Intel Corporation
+//
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM
+// Exceptions. See LICENSE.TXT
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "common.hpp"
+
+#include <vector>
+
+struct ur_sampler_handle_t_ {
+  using native_type = cl_sampler;
+  native_type Sampler;
+  ur_context_handle_t Context;
+  std::atomic<uint32_t> RefCount = 0;
+
+  ur_sampler_handle_t_(native_type Sampler, ur_context_handle_t Ctx)
+      : Sampler(Sampler), Context(Ctx) {
+    RefCount = 1;
+    urContextRetain(Context);
+  }
+
+  ~ur_sampler_handle_t_() {
+    clReleaseSampler(Sampler);
+    urContextRelease(Context);
+  }
+
+  uint32_t incrementReferenceCount() noexcept { return ++RefCount; }
+
+  uint32_t decrementReferenceCount() noexcept { return --RefCount; }
+
+  uint32_t getReferenceCount() const noexcept { return RefCount; }
+
+  native_type get() { return Sampler; }
+};

oneapi-src/level-zero-unified-runtime

source/adapters/opencl/event.hpp
@@ -1,0 +1,49 @@
+//===--------- queue.hpp - OpenCL Adapter ---------------------------===//
+//
+// Copyright (C) 2023 Intel Corporation
+//
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM
+// Exceptions. See LICENSE.TXT
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "common.hpp"
+
+#include <vector>
+
+struct ur_event_handle_t_ {
+  using native_type = cl_event;
+  native_type CLEvent;
+  ur_context_handle_t Context;
+  ur_queue_handle_t Queue;
+  std::atomic<uint32_t> RefCount = 0;
+  bool IsNativeHandleOwned = true;
+
+  ur_event_handle_t_(native_type Event, ur_context_handle_t Ctx,
+                     ur_queue_handle_t Queue)
+      : CLEvent(Event), Context(Ctx), Queue(Queue) {
+    RefCount = 1;
+    urContextRetain(Context);
+    if (Queue) {
+      urQueueRetain(Queue);
+    }
+  }
+
+  ~ur_event_handle_t_() {
+    urContextRelease(Context);
+    if (Queue) {
+      urQueueRelease(Queue);
+    }
+    if (IsNativeHandleOwned) {
+      clReleaseEvent(CLEvent);
+    }
+  }
+
+  uint32_t incrementReferenceCount() noexcept { return ++RefCount; }
+
+  uint32_t decrementReferenceCount() noexcept { return --RefCount; }
+
+  uint32_t getReferenceCount() const noexcept { return RefCount; }
+};

source/adapters/opencl/sampler.hpp
@@ -1,0 +1,41 @@
+//===--------- sampler.hpp - OpenCL Adapter ---------------------------===//
+//
+// Copyright (C) 2023 Intel Corporation
+//
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM
+// Exceptions. See LICENSE.TXT
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "common.hpp"
+
+#include <vector>
+
+struct ur_sampler_handle_t_ {
+  using native_type = cl_sampler;
+  native_type CLSampler;
+  ur_context_handle_t Context;
+  std::atomic<uint32_t> RefCount = 0;
+  bool IsNativeHandleOwned = false;
+
+  ur_sampler_handle_t_(native_type Sampler, ur_context_handle_t Ctx)
+      : CLSampler(Sampler), Context(Ctx) {
+    RefCount = 1;
+    urContextRetain(Context);
+  }
+
+  ~ur_sampler_handle_t_() {
+    urContextRelease(Context);
+    if (IsNativeHandleOwned) {
+      clReleaseSampler(CLSampler);
+    }
+  }
+
+  uint32_t incrementReferenceCount() noexcept { return ++RefCount; }
+
+  uint32_t decrementReferenceCount() noexcept { return --RefCount; }
+
+  uint32_t getReferenceCount() const noexcept { return RefCount; }
+};